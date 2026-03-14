165cd21965a900a1621ff63308a43c10ed910c97
dkalki-19 <dkalkicdhandapani@gmail.com>
2026-03-14 14:52:43 +0530

    UC2

--- hello-app/src/main/java/com/bridgelabz/hello_app/App.java
+++ hello-app/src/main/java/com/bridgelabz/hello_app/App.java
@@ -8,6 +8,6 @@ public class App {
 
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
-        System.out.println("Hello, World");
+        System.out.println("Code for UC2");
     }
 }
