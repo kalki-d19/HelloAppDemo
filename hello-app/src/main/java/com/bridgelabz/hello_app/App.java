41962f3f8f986accbbc7158e6265a26bb5c8230b
dkalki-19 <dkalkicdhandapani@gmail.com>
2026-03-14 15:05:20 +0530

    tetsting

--- hello-app/src/main/java/com/bridgelabz/hello_app/App.java
+++ hello-app/src/main/java/com/bridgelabz/hello_app/App.java
@@ -8,7 +8,6 @@ public class App {
 
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
-        System.out.println("Hello, World");
-        System.out.println("UC1");
+        int b = 10;
     }
 }
