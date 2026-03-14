13e3b9b58bdcd81c2eb739e14b12e2152dde042e
dkalki-19 <dkalkicdhandapani@gmail.com>
2026-03-14 15:09:03 +0530

    dfjh

--- hello-app/src/main/java/com/bridgelabz/hello_app/App.java
+++ hello-app/src/main/java/com/bridgelabz/hello_app/App.java
@@ -8,7 +8,7 @@ public class App {
 
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
-        int a  = 10;
+        int a = 10;
         int b = 10;
     }
 }
